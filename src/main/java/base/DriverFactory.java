457767dd842bbcbfce0eb33c227a3368c9d0ca85
Prashanth Thotakuri <prashanththotakuri30@gmail.com>
2026-01-26 17:42:58 -0500

    Create DriverFactory.java

---
+++ src/main/java/base/DriverFactory.java
@@ -0,0 +1,38 @@
+package base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverFactory {
+
+    public static WebDriver createDriver(String browser, boolean headless) {
+
+        switch (browser.toLowerCase()) {
+
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return new FirefoxDriver();
+
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                return new EdgeDriver();
+
+            case "chrome":
+            default:
+                WebDriverManager.chromedriver().setup();
+
+                ChromeOptions options = new ChromeOptions();
+
+                if (headless) {
+                    options.addArguments("--headless=new");
+                }
+
+                options.addArguments("--start-maximized");
+                return new ChromeDriver(options);
+        }
+    }
+}
